7abcd7d0deb9a7d947e160ee28575fa692c2d1b4
Wardah786 <118173570+Wardah786@users.noreply.github.com>
2022-11-21 17:11:37 +0400

    Add files via upload
    Xp_Gold

---
+++ exercise_XP Gold.py
@@ -0,0 +1,14 @@
+print(("Hello World\n"*4)+("I love python\n"*4))
+
+season = input("What is the actual month ? ")
+if season == "January" or season == "February" or season == "December":
+    print("Winter runs from December (12) to February (2)")
+elif season == "March" or season == "April" or season == "May":
+    print("Spring runs from March (3) to May (5)")
+elif season == "June" or season == "July" or season == "August":
+    print("Summer runs from June (6) to August (8)")
+elif season == "September" or season == "October" or season == "November":
+    print("Autumn runs from September (9) to November (11)")
+
+
+
